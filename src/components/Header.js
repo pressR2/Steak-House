71209732ce82c83196d1ebdd83a0ccd4a0f5161f
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-09-22 15:38:07 +0200

    refactor: remove redundant div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,13 @@ import logoBig from "../assets/l@2x.png";
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo">
-        <a href="/#">
-          <picture>
-            <source
-              media="(min-width: 800px)"
-              srcSet={`${logoBig} 2x`}
-            ></source>
-            <img src={logoSmall} alt="Bull" />
-          </picture>
-        </a>
-      </div>
+    <header className="logo">
+      <a href="/#">
+        <picture>
+          <source media="(min-width: 800px)" srcSet={`${logoBig} 2x`}></source>
+          <img src={logoSmall} alt="Bull" />
+        </picture>
+      </a>
     </header>
   );
 };
